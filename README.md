87a72f8589e49ba047692c363f97bf9e1537d149
Pruthvijit <Pruthnaik25@gmail.com>
2025-07-06 16:54:29 +0530

    Add Dark mode using system theme support

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
    - Create react project using vite and configure it 
    - Configure Tailwind CSS inside it
    - Delete unwated files
+   - Create repo on git hub , first commit and push code on it 
+- Day2
+   - Learn about Dark mode and Light mode 
+   - Toggle it using system theme support
+   - Getting stuck with manually toggle
+   - Pratice with it 
+
+  
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 
 const App = () => {
-  
-
+  document.documentElement.classList.toggle(
+  "dark",
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+);
+// Whenever the user explicitly chooses light mode
+localStorage.theme = "light";
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = "dark";
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem("theme");
   return (
-    <>
-      <p className="text-red-500">hello world</p>
-    </>
+    <div className="h-screen bg-white dark:bg-zinc-800">
+      <div className="">
+      <p className="text-green-500 dark:text-amber-200">hello world</p>
+      </div>
+   </div>
   )
 }
 
